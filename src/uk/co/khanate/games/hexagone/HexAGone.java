94226360907299d4efd16842b7b27b8f92e6becc
simon <simon@dell-desktop.example.com>
2009-05-03 16:22:29 +0100

    Changes

---
+++ src/uk/co/khanate/games/hexagone/HexAGone.java
@@ -0,0 +1,13 @@
+package uk.co.khanate.games.hexagone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HexAGone extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
