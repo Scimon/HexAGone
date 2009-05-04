242acffdb5b7f781442ba964caf2a6e1a57bf9c6
simon <simon@dell-desktop.example.com>
2009-05-04 12:59:43 +0100

    Boards

--- src/uk/co/khanate/games/hexagone/HexAGone.java
+++ src/uk/co/khanate/games/hexagone/HexAGone.java
@@ -8,6 +8,6 @@ public class HexAGone extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.board);
+        setContentView(R.layout.board_3_3);
     }
 }
