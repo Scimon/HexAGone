c623d3b470468635a625a1ff268ec21a57158fa7
simon <simon@dell-desktop.example.com>
2009-05-04 12:24:04 +0100

    Trying Layout stuff.

--- src/uk/co/khanate/games/hexagone/HexAGone.java
+++ src/uk/co/khanate/games/hexagone/HexAGone.java
@@ -8,6 +8,6 @@ public class HexAGone extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.board);
     }
 }
