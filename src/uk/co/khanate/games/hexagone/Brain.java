22cb2f3ceb3bcc1ebee22c40820612db30e6f3c4
simon <simon@dell-desktop.example.com>
2009-05-04 09:56:01 +0100

    Updates.

--- src/uk/co/khanate/games/hexagone/Brain.java
+++ src/uk/co/khanate/games/hexagone/Brain.java
@@ -1,9 +1,14 @@
 package uk.co.khanate.games.hexagone;
 
 public class Brain {
+	private Hex.hexTypes myType;
+	
+	public Brain( Hex.hexTypes t ) {
+		myType = t;
+	}
 	
 	public int[] makeMove( HexGrid g ) {
-		int[][] possibles = g.possibleMoves(Hex.hexTypes.A);
-		
+		int[][] possibles = g.possibleMoves(myType);
+		return possibles[(int) Math.random() * possibles.length];
 	}
 }
